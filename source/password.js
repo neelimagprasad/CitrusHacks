973e5bc04f0a8092b21a05a4de28831cceb9b0be
Neelima <nprasad@ucsd.edu>
2021-04-10 16:46:50 -0700

    finished the basic password checker

--- source/password.js
+++ source/password.js
@@ -1,31 +1,61 @@
 
 /* Variable that holds current password to change via input from website */
-window.onload = function () {
-    var x = document.getElementById("pwd");
-}
+console.log("helloworld");
 
+var x = document.getElementById('pwd').value;
 var checker;
-var lower;
+var lower = false;
 var upper;
 var number;
-var specialchar;
+var special;
 var length;
+var submit = document.getElementById('submit');
 /* Code for taking input from website */
+document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault();  
+    x = document.getElementById('pwd').value;
 
-checker = /[a-z]/;
-    lower = checker.test(x.value);
+    checker = /[a-z]/;
+    lower = checker.test(x);
+    checker = /[A-Z]/;
+    upper = checker.test(x);
+    checker = /[0-9]/;
+    number = checker.test(x);
+    checker = /[!@#$%^&*]/;
+    special = checker.test(x);
+    length = x.length;
     
-checker = /[A-Z]/;
-    upper = checker.test(x.value);
-checker = /[0-9]/;
-    number = checker.test(x.value);
-checker = /[!@#$%^&*]/;
-    specialchar = checker.test(x.value);
+    if(lower == false){
+        document.getElementById('message_lower').style.display= 'block';
+    }
+    else{
+        document.getElementById('message_lower').style.display= 'none';
+    }
+    if(upper == false){
+        document.getElementById('message_upper').style.display= 'block';
+    }
+    else{
+        document.getElementById('message_upper').style.display= 'none';
+    }
+    if(number == false){
+        document.getElementById('message_number').style.display= 'block';
+    }
+    else{
+        document.getElementById('message_number').style.display= 'none';
+    }
+    if(special == false){
+        document.getElementById('message_special').style.display= 'block';
+    }
+    else{
+        document.getElementById('message_special').style.display= 'none';
+    }
+    if(length < 8){
+        document.getElementById('message_length').style.display= 'block';
+    }
+    else{
+        document.getElementById('message_length').style.display= 'none';
+    }
 
-length = x.length >= 8;
-console.log(lower);
-if( lower == true ){
-    document.getElementById("message").style.display = "block";
-}
 
+});
 
