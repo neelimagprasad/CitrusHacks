60f05d06dce773da0d9e102dcae2cba3d9db545e
Neelima <nprasad@ucsd.edu>
2021-04-10 17:57:28 -0700

    finished password suggestion

--- source/password.js
+++ source/password.js
@@ -10,11 +10,12 @@ var number;
 var special;
 var length;
 var submit = document.getElementById('submit');
+var newpass;
 /* Code for taking input from website */
 document.getElementById("submit").addEventListener("click", function(event){
     event.preventDefault();  
     x = document.getElementById('pwd').value;
-
+    newpass = x
     checker = /[a-z]/;
     lower = checker.test(x);
     checker = /[A-Z]/;
@@ -27,35 +28,53 @@ document.getElementById("submit").addEventListener("click", function(event){
     
     if(lower == false){
         document.getElementById('message_lower').style.display= 'block';
+        var alphabet = "abcdefghijklmnopqrstuvwxyz";
+        var randletter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        newpass += randletter;
     }
     else{
         document.getElementById('message_lower').style.display= 'none';
     }
     if(upper == false){
         document.getElementById('message_upper').style.display= 'block';
+        var Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var randletter2 = Alphabet[Math.floor(Math.random() * Alphabet.length)];
+        newpass += randletter2;
     }
     else{
         document.getElementById('message_upper').style.display= 'none';
     }
     if(number == false){
         document.getElementById('message_number').style.display= 'block';
+        var nums = "0123456789";
+        var randnum = nums[Math.floor(Math.random() * nums.length)];
+        newpass += randnum;
     }
     else{
         document.getElementById('message_number').style.display= 'none';
     }
     if(special == false){
         document.getElementById('message_special').style.display= 'block';
+        var specialchars = "!@#$%^&*";
+        var randchar = specialchars[Math.floor(Math.random() * specialchars.length)];
+        newpass += randchar;
     }
     else{
         document.getElementById('message_special').style.display= 'none';
     }
     if(length < 8){
         document.getElementById('message_length').style.display= 'block';
+        var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+        
+        while (newpass.length < 8) {
+            newpass += chars[Math.floor(Math.random() * chars.length)];;
+            
+            }
     }
     else{
         document.getElementById('message_length').style.display= 'none';
     }
-
+    document.getElementById("suggestions").innerHTML = newpass;  
 
 });
 
